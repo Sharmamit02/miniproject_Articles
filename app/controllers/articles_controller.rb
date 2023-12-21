bca7017816aedbaed6347f55bf1e7097183e02f2
Sharmamit02 <sharmamit199701@gmail.com>
2023-12-21 13:49:42 +0530

    apply password to distroy function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,9 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "amit", password: "1234", except: [:index, :show]
+
     def index
-      @articles = Article.all
+    @articles = Article.all
     end
-  
     def show
       @article = Article.find(params[:id])
     end
